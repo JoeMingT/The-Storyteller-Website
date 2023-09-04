031654e5162ef4ecc8a397817bcb4fc9d31079c0
JoeMingT <thamjoeming@gmail.com>
2023-09-04 15:44:18 +0800

    feat/set-up-icons-and-logos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,17 @@
 import "@TheStoryteller/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <ChakraProvider>
-            <Component {...pageProps} />
-        </ChakraProvider>
+        <>
+            <Head>
+                <link rel="icon" href="/assets/favicon/favicon.ico" sizes="any" />
+            </Head>
+            <ChakraProvider>
+                <Component {...pageProps} />
+            </ChakraProvider>
+        </>
     );
 }
