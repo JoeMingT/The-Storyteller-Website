68b360988f792c2ab0ab518bba6a9e063ac28727
JoeMingT <thamjoeming@gmail.com>
2023-09-04 14:54:41 +0800

    feat/add-basic-button

---
+++ src/components/atoms/StorytellerButton/props.ts
@@ -0,0 +1,6 @@
+export interface ButtonProps  {
+  primaryColor?: string;
+  secondaryColor?: string;
+  props?: any,
+  children?: React.ReactElement | string
+}
--- src/components/atoms/index.ts
+++ src/components/atoms/index.ts
@@ -1,5 +1,5 @@
-import Button from "./Button";
+import StorytellerButton from "./StorytellerButton";
 
 export {
-  Button
+  StorytellerButton
 }
