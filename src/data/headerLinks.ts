109f4b4f52688a16127dc98f2d30e2dfc17cc131
JoeMingT <thamjoeming@gmail.com>
2023-09-24 12:36:45 +0800

    feat/added-hard-coded-links

---
+++ src/data/headerLinks.ts
@@ -0,0 +1,5 @@
+export const headerLinks = [
+    { url: "/", title: "Home" },
+    { url: "/about", title: "About Us" },
+    { url: "/galleries", title: "Galleries" },
+];
---
+++ src/data/socialMediaLinks.ts
@@ -0,0 +1,20 @@
+export const socialMediaLinks = [
+    {
+        iconSrc: "/assets/icon/instagram-black.svg",
+        iconAlt: "Instagram Icon",
+        ariaLabel: "The Storyteller Instagram",
+        url: "https://www.instagram.com/joe_thestoryteller/",
+    },
+    {
+        iconSrc: "/assets/icon/facebook-black.svg",
+        iconAlt: "Facebook Icon",
+        ariaLabel: "The Storyteller Facebook",
+        url: "https://www.facebook.com/joethestoryteller/",
+    },
+    {
+        iconSrc: "/assets/icon/xiao-hong-shu-black.svg",
+        iconAlt: "Xiao Hong SHu Icon",
+        ariaLabel: "The Storyteller Xiao Hong Shu",
+        url: "https://www.google.com",
+    },
+];
