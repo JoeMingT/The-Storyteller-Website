3b6e9fb45d6e5ef1f2099bfca8660cdb9399586d
JoeMingT <thamjoeming@gmail.com>
2023-09-24 11:21:42 +0800

    fix/theme-and-logo

--- src/theme.ts
+++ src/theme.ts
@@ -11,19 +11,19 @@ const theme = extendTheme({
     },
     backgroundColor: {},
     fontSizes: {
-        xs: "0.75rem", //12px
-        sm: "0.875rem", // 14px
-        md: "1rem", // 15px
-        lg: "1.125rem", //18px
-        xl: "1.25rem", // 20px
-        "2xl": "1.5rem", // 25px
-        "3xl": "1.875rem", // 30px
-        "4xl": "2.25rem", // 36px
-        "5xl": "2.875rem", // 46px
-        "6xl": "3.5rem", // 56px
-        "7xl": "4.5rem", // 72px
-        "8xl": "6rem", // 96px
-        "9xl": "8rem", // 128px
+        xs: "0.625rem", // 10px
+        sm: "0.75rem", // 12px
+        md: "0.875rem", // 14px
+        lg: "1rem", // 16px
+        xl: "1.125rem", // 18px
+        "2xl": "1.25rem", // 20px
+        "3xl": "1.5rem", // 25px
+        "4xl": "1.875rem", // 30px
+        "5xl": "2.25rem", // 36px
+        "6xl": "2.875rem", // 46px
+        "7xl": "3.5rem", // 56px
+        "8xl": "4.5rem", // 72px
+        "9xl": "6rem", // 96px
     },
     sizes: {
         "container.xl": "1200px",
