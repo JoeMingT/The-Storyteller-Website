2683eca901c0ae07d80d142a681515a7c75743de
JoeMingT <thamjoeming@gmail.com>
2023-09-24 18:20:47 +0800

    fix/icon-button-can-now-accept-array

--- src/components/atoms/STIconButton/index.tsx
+++ src/components/atoms/STIconButton/index.tsx
@@ -1,19 +1,28 @@
 import { IconButton, Image } from "@chakra-ui/react";
 import { STIconButtonProps } from "./props";
 
+/**
+ * The basic IconButton component for the application with the base settings.
+ * @param {STIconButtonProps} props Properties for the component
+ * @returns {React.ReactNode} The rendered component
+ */
 const STIconButton: React.FC<STIconButtonProps> = (props) => {
-    const { iconHeight, iconWidth, iconSrc, iconAlt, ...iconButtonProps } = props;
+    const { iconHeight, iconWidth, iconSrc, iconAlt, ...iconButtonProps } =
+        props;
 
     return (
         <IconButton
             height={"auto"}
             width="auto"
             background="transparent"
+            _hover={{
+                background: "transparent",
+            }}
             {...iconButtonProps}
             icon={
                 <Image
-                    w={`${iconWidth ? iconWidth : "50px"}`}
-                    h={`${iconHeight ? iconHeight : "auto"}`}
+                    w={iconWidth ? iconWidth : "50px"}
+                    h={iconHeight ? iconHeight : "auto"}
                     alt={`${iconAlt}`}
                     src={`${iconSrc}`}
                 />
--- src/components/atoms/STIconButton/props.ts
+++ src/components/atoms/STIconButton/props.ts
@@ -1,8 +1,8 @@
 import { IconButtonProps } from "@chakra-ui/react";
 
 export interface STIconButtonProps extends IconButtonProps {
-  iconWidth?: string;
-  iconHeight?: string;
-  iconSrc: string;
-  iconAlt: string;
+    iconWidth?: string | string[];
+    iconHeight?: string | string[];
+    iconSrc: string;
+    iconAlt: string;
 }
