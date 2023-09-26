9175504467a08f76b49e45da3282e62f89a89997
JoeMingT <thamjoeming@gmail.com>
2023-09-26 21:37:00 +0800

    fix/splash-image-tear-in-mobile-when-scrolling

--- src/components/molecules/SplashBackground/index.tsx
+++ src/components/molecules/SplashBackground/index.tsx
@@ -1,4 +1,5 @@
 import { Box } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 
 /**
  * Component to render the Splash image background
@@ -6,11 +7,30 @@ import { Box } from "@chakra-ui/react";
  * @returns {React.ReactNode} The Rendered Component
  */
 const SplashBackground: React.FC<any> = () => {
+    const [isMobile, setIsMobile] = useState<boolean>(false);
+
+    useEffect(() => {
+        /* Storing user's device details in a variable*/
+        let details = navigator.userAgent;
+
+        /* Creating a regular expression 
+        containing some mobile devices keywords 
+        to search it in details string*/
+        let regexp = /android|iphone|kindle|ipad/i;
+
+        /* Using test() method to search regexp in details
+        it returns boolean value*/
+        let isMobileDevice = regexp.test(details);
+
+        setIsMobile(isMobileDevice);
+    }, []);
+
     return (
         <Box
             w="100%"
             h="100vh"
             maxH={["500px", "650px"]}
+            minH={["450px", "600px"]}
             overflow="hidden"
             backgroundImage={[
                 "linear-gradient(90deg, rgba(255, 255, 255, 0.80) 100%, rgba(255, 255, 255, 0.80) 100%, rgba(255, 255, 255, 0.80) 100%, rgba(255, 255, 255, 0.80) 100%), url(/assets/images/thumbnail/thumbnail_image_1.jpg)",
@@ -18,7 +38,7 @@ const SplashBackground: React.FC<any> = () => {
             ]}
             backgroundPosition={"center"}
             backgroundRepeat={"no-repeat"}
-            backgroundAttachment={"fixed"}
+            backgroundAttachment={isMobile ? "initial" : "fixed"}
             filter={"blur(2px)"}
             flexDir="column"
             zIndex="-1"
