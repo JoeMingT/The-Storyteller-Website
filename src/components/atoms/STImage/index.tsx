879b654fcc2d805e2fd5a37e4fed2f48a3a31aaa
JoeMingT <thamjoeming@gmail.com>
2023-09-27 23:21:17 +0800

    feat/modified-stimage-behavior

--- src/components/atoms/STImage/index.tsx
+++ src/components/atoms/STImage/index.tsx
@@ -1,15 +1,16 @@
 import { useBreakpointValue } from "@chakra-ui/react";
 import NextImage from "next/image";
+import { STImageProps } from "./props";
 
 /**
  * The Image component that will be used throughout the application.
  * This utilizes the NextImage optimization + the responsiveness of ChakraUI
  * However, for important images that needs to be rendered immediately, we will not use this
  * and use regular Chakra UI Image component.
- * @param {any} props The properties of the Image. Mainly height, width, src, and alt
+ * @param {STImageProps} props The properties of the Image. Mainly height, width, src, and alt
  * @return {React.ReactNode} The rendered component
  */
-const STImage: React.FC<any> = (props) => {
+const STImage: React.FC<STImageProps> = (props) => {
     const width = useBreakpointValue(props?.width)?.replace("px", "") || 0;
     const height = useBreakpointValue(props?.height)?.replace("px", "") || 0;
 
@@ -19,6 +20,7 @@ const STImage: React.FC<any> = (props) => {
             alt={props.alt}
             width={width}
             height={height}
+            placeholder="blur"
         />
     );
 };
---
+++ src/components/atoms/STImage/props.ts
@@ -0,0 +1,8 @@
+import { ImageProps } from "@chakra-ui/react";
+
+export interface STImageProps extends ImageProps {
+    width: any;
+    height: any;
+    src: any;
+    alt: any;
+}
