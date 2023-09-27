39e3bcc4ff5efd0606036bc357348dbdbb2f0107
JoeMingT <thamjoeming@gmail.com>
2023-09-27 23:25:31 +0800

    fix/modified-splash-component

--- src/components/organisms/Splash/index.tsx
+++ src/components/organisms/Splash/index.tsx
@@ -1,10 +1,10 @@
-import { STImage } from "@StorytellerComponents/atoms";
+import StorytellerLogo from "@/public/assets/logo/storyteller_logo_rectangle.png";
+import { RedirectButton, STImage, STText } from "@StorytellerComponents/atoms";
 import {
     SplashBackground,
-    SplashCTA,
     SplashText,
 } from "@StorytellerComponents/molecules";
-import { Box, VStack } from "@chakra-ui/react";
+import { VStack } from "@chakra-ui/react";
 
 /**
  * The Splash component that is rendered at Landing Page.
@@ -14,29 +14,29 @@ const Splash: React.FC<any> = () => {
     return (
         <>
             <SplashBackground />
-            <Box>
-                <VStack
-                    w={["100%", "100%", "50%"]}
-                    h="100vh"
-                    minH={["450px", "600px"]}
-                    maxH={["500px", "650px"]}
-                    gap={["2rem", "3rem", "4rem"]}
-                    alignItems={["center", "flex-start"]}
-                    justifyContent={"center"}
-                    py={["3rem"]}
-                    px={["1rem", "0rem"]}
-                    pl={["1rem", "5rem", "7.5rem", "10rem"]}
-                >
-                    <STImage
-                        src="/assets/logo/storyteller_logo_rectangle.png"
-                        alt="Storyteller Logo"
-                        width={["150px", "200px", "325px"]}
-                        height={["1000px", "100px"]}
-                    />
-                    <SplashText />
-                    <SplashCTA />
-                </VStack>
-            </Box>
+            <VStack
+                w={["100%", "100%", "50%"]}
+                h="100vh"
+                minH={["450px", "600px"]}
+                maxH={["500px", "650px"]}
+                gap={["2rem", "3rem", "4rem"]}
+                alignItems={["center", "flex-start"]}
+                justifyContent={"center"}
+                py={["3rem"]}
+                px={["1rem", "0rem"]}
+                pl={["1rem", "5rem", "7.5rem", "10rem"]}
+            >
+                <STImage
+                    src={StorytellerLogo}
+                    alt="Storyteller Logo"
+                    width={["150px", "200px", "325px"]}
+                    height=""
+                />
+                <SplashText />
+                <RedirectButton href="/contact-us">
+                    <STText>Contact Us!</STText>
+                </RedirectButton>
+            </VStack>
         </>
     );
 };
