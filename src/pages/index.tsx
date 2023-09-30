ffaf7faed3cdf097229c0af6fe762fff63fa792d
JoeMingT <thamjoeming@gmail.com>
2023-09-30 16:31:24 +0800

    feat/landing-page-with-no-data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,32 @@
-export default function Landing() {
-  return (
-    <>
-    </>
-  )
+import {
+    AboutUs,
+    GalleryPreview,
+    Splash,
+} from "@StorytellerComponents/organisms";
+import { getProjects } from "@StorytellerSanity/queries";
+import { type GetServerSideProps } from "next";
+
+interface LandingPageProps {
+    test: string;
 }
+
+export default function Landing({ test }: LandingPageProps) {
+    return (
+        <>
+            <Splash />
+            <AboutUs />
+            <GalleryPreview />
+        </>
+    );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const data = await getProjects();
+
+    return {
+        props: {
+            test: "test text",
+            data: data,
+        },
+    };
+};
