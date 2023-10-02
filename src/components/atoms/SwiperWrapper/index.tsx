da569637dc074892ba6c314c2ef7317c308ff6eb
JoeMingT <thamjoeming@gmail.com>
2023-10-02 21:38:16 +0800

    feat/added-dynamic-layout-for-swiper-wrapper

--- src/components/atoms/SwiperWrapper/index.tsx
+++ src/components/atoms/SwiperWrapper/index.tsx
@@ -6,6 +6,7 @@ import { Swiper } from "swiper/react";
 import "swiper/swiper-bundle.css";
 
 import { STIconButton } from "@StorytellerComponents/atoms";
+import { Box } from "@chakra-ui/react";
 import { useRef } from "react";
 import { SwiperWrapperProps } from "./props";
 
@@ -13,42 +14,51 @@ import { SwiperWrapperProps } from "./props";
  * The Swiper component that wraps around SwiperSlides with a set of default settings.
  * It also include 2 custom buttons for the Swiper that is positioned within the Swiper.
  * Is used whenever we want to utilize Swiper package.
+ * Wrap the entire thing inside a box. By changing the "display" properties of the box and "position" propoerties of the buttons,
+ * we can modify the entire layout of the swiper.
  *
- * @param {SwiperWrapperProps} props The properties for the component. The children will be wrapped around by the component while the leftButtonProps and rightButtonProps is used to modify the navigation buttons
+ * @param {SwiperWrapperProps} props The properties for the component. The children will be wrapped around by the component while the leftButtonProps and rightButtonProps is used to modify the navigation buttons. Box Props is used to allow for different swiper layouts
  * @returns {React.ReactNode} The Swiper wrapping around SwiperSlides
  */
 const SwiperWrapper: React.FC<SwiperWrapperProps> = (props) => {
     const prevButtonRef = useRef<HTMLButtonElement>(null);
     const nextButtonRef = useRef<HTMLButtonElement>(null);
 
-    const { children, leftButtonProps, rightButtonProps, ...swiperProps } =
-        props;
+    const {
+        children,
+        leftButtonProps,
+        rightButtonProps,
+        boxProps,
+        ...swiperProps
+    } = props;
     return (
-        <Swiper
-            breakpoints={{}}
-            modules={[Navigation, Pagination, A11y, Autoplay]}
-            navigation={{
-                prevEl: prevButtonRef.current,
-                nextEl: nextButtonRef.current,
-            }}
-            pagination={{
-                clickable: true,
-            }}
-            onInit={(swiper: any) => {
-                swiper.params.navigation.prevEl = prevButtonRef.current;
-                swiper.params.navigation.nextEl = nextButtonRef.current;
-                swiper.navigation.init();
-                swiper.navigation.update();
-            }}
-            loop={true}
-            autoplay={{ delay: 5000 }}
-            style={{
-                width: "100%",
-                display: "flex",
-                alignItems: "center",
-            }}
-            {...swiperProps}
-        >
+        <Box display="flex" alignItems="center" {...boxProps}>
+            <Swiper
+                modules={[Navigation, Pagination, A11y, Autoplay]}
+                navigation={{
+                    prevEl: prevButtonRef.current,
+                    nextEl: nextButtonRef.current,
+                }}
+                pagination={{
+                    clickable: true,
+                }}
+                onInit={(swiper: any) => {
+                    swiper.params.navigation.prevEl = prevButtonRef.current;
+                    swiper.params.navigation.nextEl = nextButtonRef.current;
+                    swiper.navigation.init();
+                    swiper.navigation.update();
+                }}
+                loop={true}
+                autoplay={{ delay: 5000 }}
+                style={{
+                    width: "100%",
+                    display: "flex",
+                    alignItems: "center",
+                }}
+                {...swiperProps}
+            >
+                {children}
+            </Swiper>
             {/* Absolute buttons to include it within the swiper */}
             <STIconButton
                 position="absolute"
@@ -57,7 +67,6 @@ const SwiperWrapper: React.FC<SwiperWrapperProps> = (props) => {
                 ref={prevButtonRef}
                 {...leftButtonProps}
             />
-            {children}
             <STIconButton
                 position="absolute"
                 zIndex="2"
@@ -65,7 +74,7 @@ const SwiperWrapper: React.FC<SwiperWrapperProps> = (props) => {
                 ref={nextButtonRef}
                 {...rightButtonProps}
             />
-        </Swiper>
+        </Box>
     );
 };
 
--- src/components/atoms/SwiperWrapper/props.ts
+++ src/components/atoms/SwiperWrapper/props.ts
@@ -1,8 +1,10 @@
 import { SwiperProps } from "swiper/react";
 import { STIconButtonProps } from "../STIconButton/props";
+import { BoxProps } from "@chakra-ui/react";
 
 export interface SwiperWrapperProps extends SwiperProps {
     children: React.ReactNode;
     leftButtonProps?: STIconButtonProps;
     rightButtonProps?: STIconButtonProps;
+    boxProps?: BoxProps;
 }
