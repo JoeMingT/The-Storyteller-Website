ba45860ebf369a13bc42c03d80d04314ee87c008
JoeMingT <thamjoeming@gmail.com>
2023-10-22 15:48:48 +0800

    feat/added-and-modified-test-data

---
+++ src/data/testAllGallery.ts
@@ -0,0 +1,48 @@
+export const allGalleryTestData = [
+    {
+        imgUrl: "/assets/images/gallery-thumbnail/horizontal.jpg",
+        galleryTitle: "Tropical Theme Garden Wedding at The Saujana Hotel, KL",
+        galleryUrl: "#",
+    },
+    {
+        imgUrl: "/assets/images/gallery-thumbnail/vertical.jpg",
+        galleryTitle: "Garden Wedding at Janda Baik, Tanarimba",
+        galleryUrl: "#",
+    },
+    {
+        imgUrl: "/assets/images/gallery-thumbnail/horizontal.jpg",
+        galleryTitle:
+            "This is a very long title in order to see how the website would react when the text overflows into a new layer. Maybe we can try limiting the amount of words that are being displayed on the title so it won't become too long and ruin how it would look on both the desktop and mainly the mobile view. The mobile view is the main concern right now and I'm not sure what it would look like with such a long title.",
+        galleryUrl: "#",
+    },
+    {
+        imgUrl: "/assets/images/gallery-thumbnail/vertical.jpg",
+        galleryTitle: "Test Title",
+        galleryUrl: "#",
+    },
+    {
+        imgUrl: "/assets/images/gallery-thumbnail/vertical.jpg",
+        galleryTitle: "Test Title",
+        galleryUrl: "#",
+    },
+    {
+        imgUrl: "/assets/images/gallery-thumbnail/vertical.jpg",
+        galleryTitle: "Test Title",
+        galleryUrl: "#",
+    },
+    {
+        imgUrl: "/assets/images/gallery-thumbnail/vertical.jpg",
+        galleryTitle: "Test Title",
+        galleryUrl: "#",
+    },
+    {
+        imgUrl: "/assets/images/gallery-thumbnail/vertical.jpg",
+        galleryTitle: "Test Title",
+        galleryUrl: "#",
+    },
+    {
+        imgUrl: "/assets/images/gallery-thumbnail/vertical.jpg",
+        galleryTitle: "Test Title",
+        galleryUrl: "#",
+    },
+];
