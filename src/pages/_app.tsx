0710b9d6b118251019b3e44fb36dfe967b159279
JoeMingT <thamjoeming@gmail.com>
2024-01-06 10:21:34 +0800

    feat/change-title-description-added-keywords

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ export default function App({ Component, pageProps }: AppProps) {
     return (
         <>
             <Head>
-                <title>The Storyteller by Joe</title>
+                <title>The Storyteller Studio Malaysia</title>
                 <link
                     rel="icon"
                     href="/assets/favicon/favicon.ico"
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 />
                 <meta
                     name="keyword"
-                    content="TheStoryteller, TheStoryteller by Joe, Tham Joe Ping, Photography, Studio, Photographer, Photographers, Photography Studio, Professional Photography"
+                    content="The Storyteller Studio Malaysia, TheStoryteller Malaysia, The Storyteller Malaysia, TheStorytellerStudios Malaysia, TheStoryteller, TheStoryteller Studios, TheStorytellerStudios, TheStoryteller by Joe, The Storyteller by Joe, The Storyteller Studios by Joe, Tham Joe Ping, Photography, Studio, Photographer, Photographers, Photography Studio, Professional Photography, Videographer, Videography, Professional Videography, Video Editing, Photo Editing, Wedding Photos, Graduation Photos, Malaysia, Selangor, Kuala Lumpur, Johor, Singapore, Professional Services"
                 />
                 <meta
                     name="viewport"
@@ -25,7 +25,7 @@ export default function App({ Component, pageProps }: AppProps) {
                 />
                 <meta
                     name="description"
-                    content="The Storyteller Studio is a Photography and Videography studio that provides a variety of photography and videography services for weddings, graduations, portraits, events, and more for clients located in Malaysia mainly in Melaka, Johor Bahru, Kuala Lumpur, and Singapore. We also provide services such as video editing for wedding, photos editing and graduation robe rental. Founded in 2017, the company is lead by Joe who has more than 10 years of experience in photography and videography. We are looking forward to capture your memorable moments!"
+                    content="The Storyteller Studio Malaysia is a Photography and Videography studio that provides a variety of photography and videography services for weddings, graduations, portraits, events, and more for clients located in Malaysia mainly in Melaka, Johor Bahru, Kuala Lumpur, and Singapore. We also provide services such as video editing for wedding, photos editing and graduation robe rental. Founded in 2017, the company is lead by Joe who has more than 10 years of experience in photography and videography. We are looking forward to capture your memorable moments!"
                 />
             </Head>
             <ChakraProvider theme={theme}>
