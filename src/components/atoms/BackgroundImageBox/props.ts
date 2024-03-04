26aa53134eb3a93c7218df046a47adb8f9b746e0
JoeMingT <thamjoeming@gmail.com>
2024-03-04 14:26:19 +0800

    feat/allow-children-in-background-image-box

--- src/components/atoms/BackgroundImageBox/props.ts
+++ src/components/atoms/BackgroundImageBox/props.ts
@@ -2,4 +2,5 @@ import { BoxProps } from "@chakra-ui/react";
 
 export interface BackgroundImageBoxProps extends BoxProps {
     backgroundImage: string | string[];
+    children?: React.ReactNode | string;
 }
