4b09bfd14148b80d91ba19a72721c934dcc476ee
JoeMingT <thamjoeming@gmail.com>
2024-03-04 14:25:55 +0800

    feat/static-data-update

--- src/data/headerLinks.ts
+++ src/data/headerLinks.ts
@@ -2,4 +2,5 @@ export const headerLinks = [
     { url: "/", title: "Home" },
     { url: "/about", title: "About Us" },
     { url: "/gallery", title: "Galleries" },
+    { url: "/contact-us", title: "Contact Us" },
 ];
